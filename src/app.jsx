248517e2a8fb125da0e2a4c4d861591256efa4f2
joao-ressel <joao.resselti@gmail.com>
2024-10-29 16:20:04 -0500

    feat: install react-router-dom, styled-components, create pages and config app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { Feed } from "./pages/feed";
+import { GlobalStyle } from "./style";
 
 export const App = () => {
-
   return (
-    <h1>
-     
-        DIO CLONE
-    </h1>
+    <Router>
+      <GlobalStyle/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/feed" element={<Feed />} />
+      </Routes>
+    </Router>
   );
 };
-
-export default App;
